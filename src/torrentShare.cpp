59ad258e042424f417ccb2dc91856507f6ef83c7
Max <marlier.maxime@gmail.com>
2015-01-13 12:12:10 +0100

    [add] draft for torrentShare implementation

--- src/torrentShare.cpp
+++ src/torrentShare.cpp
@@ -6,19 +6,26 @@ torrentShare::torrentShare(){
 
 
 void torrentShare::update(){
-
+	for(int i=0; i<nw.size() ; i++)
+		nw[i]->update();
+	for(int i=0; i<packet.size() ; i++)
+		packet[i]->update();
 }
 
 void torrentShare::draw(){
-
+	for(int i=0; i<nw.size() ; i++)
+		nw[i]->draw();
+	for(int i=0; i<packet.size() ; i++)
+		packet[i]->draw();
 }
 
 void torrentShare::addNode(){
-
+	nw.push_back(new btNode());
 }
 
-void torrentShare::removeNode(){
-
+void torrentShare::removeNode(int index){
+	delete nw[index]; // free memory
+	nw[index] = 0; // ptr == 0
 }
 
 void torrentShare::sendPacket(){
